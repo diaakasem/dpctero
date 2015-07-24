0e1ff3ed45aa1aa468cec3ba700366f906e889b6
Diaa Kasem <me@diaa.me>
2015-07-24 18:45:29 +0200

    Updating the base setup

--- datasheild/datasheild/settings.py
+++ datasheild/datasheild/settings.py
@@ -96,9 +96,18 @@ WSGI_APPLICATION = 'datasheild.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME', 'datashield'),
+        'USER': os.environ.get('DATABASE_USERNAME', 'datagurdian'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', '123456'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
